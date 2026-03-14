949fb478ca38ece32623f36a75c40d091f5dc35b
LeTran <lethanhbaotran.letran@gmail.com>
2026-03-14 19:19:51 +0700

    SCRUM-92 feature(context): API phân tích tài liệu và đưa ra context

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from app.database import init_db
 from app.routers import topics
 
 app = FastAPI(title="VR Presentation Trainer API")
-
-@app.on_event("startup")
-def on_startup():
-    init_db()
 
 # Đăng ký router
 app.include_router(topics.router, prefix="/api/v1", tags=["Context Upload"])
--- backend/app/services/workflow.py
+++ backend/app/services/workflow.py
@@ -1,20 +1,12 @@
-import uuid
 from fastapi import UploadFile, HTTPException
-from sqlalchemy.orm import Session
 from app.services.file_processor import extract_pdf, read_txt
 from app.services.ai_service import clean_and_summarize
-from app.crud import create_topic
-from app.services.storage_service import save_upload_file
+from app.schemas import TopicResponse
 
 async def process_presentation_upload(
-    db: Session, 
-    title: str,
-    description: str,
     slide_file: UploadFile, 
     script_file: UploadFile | None
-):
-    topic_id = str(uuid.uuid4())
-    
+):  
     # Validate (Logic nghiệp vụ)
     if not slide_file.filename.endswith('.pdf'):
         raise HTTPException(status_code=400, detail="Slide file must be PDF")
@@ -32,21 +24,17 @@ async def process_presentation_upload(
     
     if not slide_text and not script_text:
         raise HTTPException(status_code=400, detail="No content extracted")
-    
-    saved_slide_path = await save_upload_file(slide_file, topic_id, "pdf")
-    
-    saved_script_path = None
-    if script_file:
-        saved_script_path = await save_upload_file(script_file, topic_id, "txt")
 
     # Merge Logic
-    full_text = f"---TITLE---{title}\n---DESCRIPTION---\n{description}---SLIDE---\n{slide_text}\n---SCRIPT---\n{script_text}"
+    full_text = f"---SLIDE---\n{slide_text}\n---SCRIPT---\n{script_text}"
 
     # AI Processing
-    summary = await clean_and_summarize(full_text)
+    ai_result = await clean_and_summarize(full_text)
     
-    # Persist Data (Gọi CRUD)
-    new_session = create_topic(db, title=title, description=description, context=summary, slide_path=saved_slide_path,
-        script_path=saved_script_path, topic_id=topic_id)
+    topic = TopicResponse(
+        title=ai_result["title"],
+        description=ai_result["description"],
+        context_text=ai_result["context_text"]
+    )
     
-    return new_session
+    return topic
