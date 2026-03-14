d1611c57973866d76dec650717b0edfa6f59bcbd
LeTran <lethanhbaotran.letran@gmail.com>
2026-03-15 00:20:14 +0700

    SCRUM-95 feat(Dashboard): chuyển thành teleport

---
+++ vr-presentation-trainer/Assets/Scripts/PositionChanger.cs
@@ -0,0 +1,31 @@
+using Assets.CustomPdfViewer.Scripts;
+using UnityEngine;
+public class PositionChanger : MonoBehaviour
+{
+    public void GoToMainRoom()
+    {
+        Debug.Log("Đang dịch chuyển VRPlayer vào phòng chính...");
+
+        // Tìm object có tên chính xác là "VRPlayer"
+        GameObject player = GameObject.Find("VR Player");
+
+        if (player != null)
+        {
+            // Dịch chuyển tức thời đến tọa độ x=-3.5, y=0, z=1
+            player.transform.position = new Vector3(-3.5f, 0f, 1f);
+
+            CustomPdfViewerUI pdfViewer = FindFirstObjectByType<CustomPdfViewerUI>();
+            if (pdfViewer != null)
+            {
+                pdfViewer.EnablePresentationMode();
+            }
+
+            Debug.Log("Dịch chuyển thành công!");
+        }
+        else
+        {
+            // Báo lỗi đỏ chót dưới Console nếu gõ sai tên
+            Debug.LogError("❌ Không tìm thấy object nào tên là VRPlayer cả! Bạn check lại tên bên Hierarchy nha.");
+        }
+    }
+}
