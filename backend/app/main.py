0d5a10e3569f31398d115167ed0a4315e0e55a49
LeTran <lethanhbaotran.letran@gmail.com>
2026-03-15 02:13:37 +0700

    SCRUM-95 feat(API): Tích hợp gọi API phân tích

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routers import topics
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="VR Presentation Trainer API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Cho phép mọi tên miền gọi API
+    allow_credentials=True,
+    allow_methods=["*"], # Cho phép mọi phương thức (GET, POST...)
+    allow_headers=["*"], # Cho phép mọi headers (bao gồm cả header của ngrok)
+)
 
 # Đăng ký router
 app.include_router(topics.router, prefix="/api/v1", tags=["Context Upload"])
